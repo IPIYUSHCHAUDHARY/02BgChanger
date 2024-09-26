41031745d5b5df90a5c8966ea5d5b8aecc4fa84f
ipiyushchaudhary <ipiyushchaudhary@gmail.com>
2024-09-26 11:05:09 +0530

    resolve background change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import ColorCard from './components/ColorCard'
 
-const colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink' ]
+const colorList = ['bg-red-500', 'bg-orange-500', 'bg-yellow-500', 'bg-green-500', 'bg-blue-500', 'bg-purple-500', 'bg-pink-500' ]
 
 function App() {
   const [background, setBackground] = useState("bg-red-500")
 
   function handleColorChange(bg) {
-    setBackground('bg-'+bg+'-500');    
+    setBackground(bg);    
   }
 
   return (
--- src/components/ColorCard.jsx
+++ src/components/ColorCard.jsx
@@ -2,7 +2,7 @@ function ColorCard({colors, OnClick}) {
   return (        
     <div className="flex justify-center gap-2 rounded-lg border border-gray-100 bg-gray-100 p-2 absolute bottom-3 left-3 right-3">
         {colors.map(color => 
-          <button className={`inline-block rounded-md px-4 py-2 text-sm text-${color}-500 hover:text-gray-700 bg-gray-300 focus:relative`} key={color} onClick={() => OnClick(color)}>{color}</button>
+          <button className={`inline-block rounded-md px-4 py-2 text-sm text-white hover:text-gray-700 bg-gray-300 focus:relative capitalize ${color}`} key={color} onClick={() => OnClick(color)}>{color.split('-')[1]}</button>
         )}
     </div>
   )
